7fabd4f7cf8180d267eb1eaab91591db15f08253
James Riach <james@jriach.co.uk>
2020-07-08 15:23:57 +0100

    Add project files.

---
+++ PSSNMPAgent.Common.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+using System.Collections.ObjectModel;
+
+namespace PSSNMPAgent.Common
+{
+    public class SNMPCommunity
+    {
+        public string Community { get; set; }
+        public string AccessRights { get; set; }
+    }
+
+    public class SNMPAgent
+    {
+        public string SNMPHosts { get; set; }
+        public string TrapCommunities { get; set; }
+        public string TrapHosts { get; set; }
+        public bool EnableAuthTraps { get; set; }
+        public int NameResolutionRetries { get; set; }
+        public string SysContact { get; set; }
+        public string SysLocation { get; set; }
+    }
+
+    public class SNMPAgentCommon
+    {
+        private string regRootSubKey = @"SYSTEM\CurrentControlSet\Services\SNMP\Parameters";
+        private string regCommunities = @"SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities";
+
+        public string RegRootSubKey
+        {
+            get { return regRootSubKey; }
+        }
+
+        public string RegCommunities
+        {
+            get { return regCommunities; }
+        }
+
+        public static void ServiceCheck()
+        {
+            var Services = ServiceController.GetServices();
+            int SvcMatch = 0;
+            foreach (var Service in Services)
+            {
+                if (Service.ServiceName == "SNMP")
+                {
+                    SvcMatch++;
+                    break;
+                }
+            }
+
+            if (SvcMatch == 0)
+            {
+                throw new Exception("SNMP Service is Not Installed");
+            }
+        }
+
+        public class communityAccess
+        {
+            public string Access;
+            public int dWordVal;
+        }
+
+        private List<communityAccess> _communityAccess = new List<communityAccess>
+        {
+            new communityAccess() { Access = "None", dWordVal = 1 },
+            new communityAccess() { Access = "Notify", dWordVal = 2},
+            new communityAccess() { Access = "ReadOnly", dWordVal = 4},
+            new communityAccess() { Access = "ReadWrite", dWordVal = 8},
+            new communityAccess() { Access = "ReadCreate", dWordVal = 16}
+        };
+
+        public ReadOnlyCollection<communityAccess> CommunityAccess
+        {
+            get { return new ReadOnlyCollection<communityAccess>(_communityAccess); }
+        }
+    }
+}
---
+++ PSSNMPAgent.GetSNMPCommunity.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using PSSNMPAgent.Common;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using System.IO.Pipes;
+
+namespace GetSNMPCommunities.Cmd
+{
+    [Cmdlet(VerbsCommon.Get, nameof(SNMPCommunity))]
+    [OutputType(typeof(SNMPCommunity))]
+    public class GetSNMPCommunities: PSCmdlet
+    {
+        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "Get SNMP Communities")]
+        [AllowNull]
+        public string[] Communities { get; set; }
+
+        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "Get SNMP Communities by Access Right")]
+        [ValidateSet("None", "Notify", "ReadOnly", "ReadWrite", "ReadCreate")]
+        public string[] AccessRights { get; set; }
+
+        private static IEnumerable<SNMPCommunity> _SNMPCommunities;
+
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("Checking SNMP Service is installed...");
+            SNMPAgentCommon.ServiceCheck();
+
+            _SNMPCommunities = GetCommunities();
+
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            var results = _SNMPCommunities;
+
+            if (Communities != null)
+            {
+                results = results.Where(community => Regex.IsMatch(community.Community, string.Format("^(?:{0})", string.Join("|", Communities))));
+            }
+
+            if (MyInvocation.BoundParameters.ContainsKey("AccessRights"))
+            {
+                results = results.Where(access => Regex.IsMatch(access.AccessRights, string.Format("^(?:{0})", string.Join("|", AccessRights))));
+            }
+
+            results.ToList().ForEach(WriteObject);           
+
+            base.ProcessRecord();
+        }
+
+        private static IEnumerable<SNMPCommunity> GetCommunities()
+        {
+            SNMPAgentCommon common = new SNMPAgentCommon();
+            RegistryKey RegCommunities = Registry.LocalMachine.OpenSubKey(common.RegCommunities);
+
+            List<SNMPCommunity> communities = new List<SNMPCommunity>();
+            
+            foreach (string Community in RegCommunities.GetValueNames())
+            {
+                int accessValue = (int)RegCommunities.GetValue(Community);
+                var accessType = common.CommunityAccess.Single(a => a.dWordVal == accessValue);
+                string access = accessType.Access;
+                communities.Add(new SNMPCommunity { Community = Community, AccessRights = access });
+            }
+            RegCommunities.Close();
+
+            return communities;
+        }        
+    }
+}
+
