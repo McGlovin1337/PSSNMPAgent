6a441ea0d267adaa9d588b8c3494dbbf72fb8991
James Riach <james@jriach.co.uk>
2020-07-11 01:22:52 +0100

    2020-07-10 Added Add-SNMPHost cmdlet Added Remove-SNMPHost cmdlet

---
+++ PSSNMPAgent/PSSNMPAgent.AddSNMPHost.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using PSSNMPAgent.Common;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+
+namespace AddSNMPHost.cmd
+{
+    [Cmdlet(VerbsCommon.Add, nameof(SNMPHost))]
+    [OutputType(typeof(SNMPHost))]
+
+    public class AddSNMPHost: PSCmdlet
+    {
+        [Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, Mandatory = true, HelpMessage = "Add a new SNMP Permitted Manager")]
+        public string[] Hosts { get; set; }
+
+        private static IEnumerable<SNMPHost> _SNMPHosts;
+
+        protected override void BeginProcessing()
+        {
+            if (MyInvocation.BoundParameters.ContainsKey("Hosts"))
+            {
+                foreach (string Host in Hosts)
+                {
+                    var Match = Regex.Match(Host, @"^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$");
+                    if (!Match.Success)
+                    {
+                        throw new ArgumentException("Specified Host is not a valid hostname: " + Host);
+                    }
+                }
+            }
+            WriteVerbose("Checking SNMP Service is installed...");
+            SNMPAgentCommon.ServiceCheck();
+
+            _SNMPHosts = GetSNMPHosts();            
+
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            var results = _SNMPHosts;
+
+            string[] LowerHost = Array.ConvertAll(Hosts, host => host.ToLower());
+
+            if (results.Count() > 0)
+            {
+                results = results.Where(result => LowerHost.Contains(result.Host.ToLower()));
+            }
+
+            if (results.Count() > 0)
+            {
+                throw new Exception("SNMP Host already exists");
+            }
+
+            WriteVerbose("Adding " + Hosts.Count() + " hosts...");
+            AddSNMPHosts(Hosts);
+
+            _SNMPHosts = GetSNMPHosts();
+
+            base.ProcessRecord();
+        }
+
+        protected override void EndProcessing()
+        {
+            var results = _SNMPHosts;
+            string[] LowerHost = Array.ConvertAll(Hosts, host => host.ToLower());
+
+            results = results.Where(result => LowerHost.Contains(result.Host.ToLower()));
+
+            results.ToList().ForEach(WriteObject);
+
+            base.EndProcessing();
+        }
+
+        private static IEnumerable<SNMPHost> GetSNMPHosts()
+        {
+            SNMPAgentCommon common = new SNMPAgentCommon();
+            RegistryKey RegHosts = Registry.LocalMachine.OpenSubKey(common.RegHosts);
+
+            List<SNMPHost> hosts = new List<SNMPHost>();
+
+            foreach (string value in RegHosts.GetValueNames())
+            {
+                string host = (string)RegHosts.GetValue(value);
+                hosts.Add(new SNMPHost { Host = host });
+            }
+            RegHosts.Close();
+
+            return hosts;
+        }
+
+        private static void AddSNMPHosts(string[] Hosts)
+        {
+            SNMPAgentCommon common = new SNMPAgentCommon();
+            RegistryKey RegHosts = Registry.LocalMachine.OpenSubKey(common.RegHosts);
+            List<int> valueNames = new List<int>();
+
+            foreach (string valueName in RegHosts.GetValueNames())
+            {
+                if (valueName != "(Default)")
+                {
+                    valueNames.Add(Convert.ToInt32(valueName));
+                }
+            }
+            RegHosts.Close();
+
+            RegHosts = Registry.LocalMachine.CreateSubKey(common.RegHosts);
+            int val = 1;
+            String Value;
+            bool valExist = true;
+            foreach (var Host in Hosts)
+            {
+                while (valExist == true)
+                {
+                    valExist = valueNames.Contains(val);
+                    if (valExist == true) { val++; }
+                }
+                Value = Convert.ToString(val);
+                RegHosts.SetValue(Value, Host);
+                valueNames.Add(val);
+                valExist = true;
+                val = 1;
+            }
+            RegHosts.Close();
+        }
+    }
+}
---
+++ PSSNMPAgent/PSSNMPAgent.RemoveSNMPHost.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using PSSNMPAgent.Common;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+
+namespace RemoveSNMPHost.cmd
+{
+    [Cmdlet(VerbsCommon.Remove, nameof(SNMPHost))]
+    public class RemoveSNMPHost: PSCmdlet
+    {
+        [Parameter(Position = 0, ParameterSetName = "Hosts", ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, Mandatory = true, HelpMessage = "Remove SNMP Permitted Managers")]
+        public string[] Hosts { get; set; }
+
+        [Parameter(Position = 1, ParameterSetName = "RemoveAll", ValueFromPipelineByPropertyName = true, Mandatory = true, HelpMessage = "Remove all SNMP Permitted Managers")]
+        public SwitchParameter RemoveAllHosts { get; set; }
+
+        private static IEnumerable<SNMPHost> _SNMPHosts;
+
+        protected override void BeginProcessing()
+        {
+            if (MyInvocation.BoundParameters.ContainsKey("Hosts"))
+            {
+                foreach (string Host in Hosts)
+                {
+                    var Match = Regex.Match(Host, @"^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$");
+                    if (!Match.Success)
+                    {
+                        throw new ArgumentException("Specified Host is not a valid hostname: " + Host);
+                    }
+                }
+            }
+            WriteVerbose("Checking SNMP Service is installed...");
+            SNMPAgentCommon.ServiceCheck();
+
+            _SNMPHosts = GetSNMPHosts();
+
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            var results = _SNMPHosts;
+
+            if (results.Count() == 0)
+            {
+                throw new Exception("Host list is empty, no hosts to remove");
+            }
+
+            if (MyInvocation.BoundParameters.ContainsKey("RemoveAllHosts"))
+            {
+                RemoveSNMPHosts(results);
+            }
+            else
+            {
+                string[] LowerHost = Array.ConvertAll(Hosts, host => host.ToLower());
+
+                results = results.Where(result => LowerHost.Contains(result.Host.ToLower()));
+
+                if (results.Count() > 0 && results.Count() <= Hosts.Count())
+                {
+                    RemoveSNMPHosts(results);
+                }
+                else
+                {
+                    throw new Exception("None of the specified hosts were found");
+                }
+            }
+
+            _SNMPHosts = GetSNMPHosts();
+
+            base.ProcessRecord();
+        }
+
+        protected override void EndProcessing()
+        {
+            var results = _SNMPHosts;
+
+            results.ToList().ForEach(WriteObject);
+
+            if (MyInvocation.BoundParameters.ContainsKey("Hosts"))
+            {
+                string[] LowerHost = Array.ConvertAll(Hosts, host => host.ToLower());
+
+
+                if (results.Count() > 0)
+                {
+                    results = results.Where(result => LowerHost.Contains(result.Host.ToLower()));
+                }
+            }
+
+            if (results.Count() == 0)
+            {
+                WriteVerbose("Specified hosts removed");
+            }
+            else
+            {
+                throw new Exception("Some hosts failed to remove");
+            }
+
+            base.EndProcessing();
+        }
+
+        private static IEnumerable<SNMPHost> GetSNMPHosts()
+        {
+            SNMPAgentCommon common = new SNMPAgentCommon();
+            RegistryKey RegHosts = Registry.LocalMachine.OpenSubKey(common.RegHosts);
+
+            List<SNMPHost> hosts = new List<SNMPHost>();
+
+            foreach (string value in RegHosts.GetValueNames())
+            {
+                string host = (string)RegHosts.GetValue(value);
+                hosts.Add(new SNMPHost { Host = host });
+            }
+            RegHosts.Close();
+
+            return hosts;
+        }
+
+        private static void RemoveSNMPHosts(IEnumerable<SNMPHost> Hosts)
+        {
+            SNMPAgentCommon common = new SNMPAgentCommon();
+            RegistryKey RegHosts = Registry.LocalMachine.OpenSubKey(common.RegHosts);
+            List<string> valueNames = new List<string>();
+            string[] LowerHosts = Hosts.Select(host => host.Host).ToArray();
+            LowerHosts = Array.ConvertAll(LowerHosts, host => host.ToLower());
+            string hostname;
+
+            foreach (string valueName in RegHosts.GetValueNames())
+            {
+                if (valueName != "(Default)")
+                {
+                    hostname = (string)RegHosts.GetValue(valueName);
+                    if (LowerHosts.Contains(hostname.ToLower()))
+                    {
+                        valueNames.Add(valueName);
+                    }
+                }
+            }
+            RegHosts.Close();
+
+            RegHosts = Registry.LocalMachine.CreateSubKey(common.RegHosts);
+            foreach (string value in valueNames)
+            {
+                RegHosts.DeleteValue(value);
+            }
+            RegHosts.Close();
+        }
+    }
+}
